1d087978c9837370e902a3592753fa43ce195ed1
JT Marquez <marquezjt8@gmail.com>
2026-04-27 18:40:25 -0700

    18:40

--- PA9_football/main.cpp
+++ PA9_football/main.cpp
@@ -1,1 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*	Programmers: Josh Marquez, Iyke Odum												 *
+*	Class: CptS 122, Spring 2026  Lab Section 1											 *
+*	Programming Assignment: PA 9 2D Football Game Application							 *
+*	Date: April 27, 2026																 *
+*	Description: This program runs a simple 2D football game.							 *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include "Player.hpp"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
